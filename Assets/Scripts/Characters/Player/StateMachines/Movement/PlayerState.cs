6613375c7b7fde70cd13aa10be65e32852471754
Reynard Tjong <S10245153@connect.np.edu.sg>
2024-08-06 05:15:30 +0800

    Player reconfiguration

---
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/PlayerState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BladesOfDeceptionCapstoneProject
+{
+    public class PlayerState : ScriptableObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
--- Assets/Scripts/Characters/Player/StateMachines/StateMachine/StateMachine.cs
+++ Assets/Scripts/Characters/Player/StateMachines/StateMachine/StateMachine.cs
@@ -15,6 +15,4 @@ public class StateMachine
         currentState = newState;
         newState.Enter();
     }
-
-
 }
