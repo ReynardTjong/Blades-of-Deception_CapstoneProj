b69b85a640d66e2309c8c5a6d7e1039f20bde6b3
Reynard Tjong <S10245153@connect.np.edu.sg>
2024-08-14 02:30:50 +0800

    stuffs

--- Assets/Scripts/Characters/Version2/AI/Manager/EnemyManager.cs
+++ Assets/Scripts/Characters/Version2/AI/Manager/EnemyManager.cs
@@ -18,7 +18,6 @@ namespace BladesOfDeceptionCapstoneProject
 
         public bool isPerformingAction;
         public bool isInteracting;
-        public float distanceFromTarget;
         public float rotationSpeed = 25;
         public float maximumAttackRange = 1.5f;
 
@@ -26,7 +25,6 @@ namespace BladesOfDeceptionCapstoneProject
         public float detectionRadius = 20;
         public float maximumDetectionAngle = 50;
         public float minimumDetectionAngle = -50;
-        public float viewableAngle;
 
         public float currentRecoveryTime = 0;
 
